cf9480244a5d5d445d9264922313c69dc3bd15f2
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2025-02-23 11:47:58 +0000

    feat: connect with supabase storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -33,7 +34,34 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: {
+          prefix: 'media',
+        },
+      },
+      bucket:
+        process.env.S3_BUCKET ??
+        (() => {
+          throw new Error('Missing environment variable: S3_BUCKET')
+        })(),
+      config: {
+        forcePathStyle: true,
+        credentials: {
+          accessKeyId:
+            process.env.S3_ACCESS_KEY_ID ??
+            (() => {
+              throw new Error('Missing environment variable: S3_ACCESS_KEY_ID')
+            })(),
+          secretAccessKey:
+            process.env.S3_SECRET_ACCESS_KEY ??
+            (() => {
+              throw new Error('Missing environment variable: S3_SECRET_ACCESS_KEY')
+            })(),
+        },
+        region: process.env.S3_REGION || 'us-east-1', // Default region if not provided
+        endpoint: process.env.S3_ENDPOINT,
+      },
+    }),
   ],
 })
